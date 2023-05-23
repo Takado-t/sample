76d663676b963e7a0fad0cb8424699e23bec25ae
Takado-t <traveling.g.0415@gmail.com>
2023-05-23 14:42:09 +0900

    適当なコメント文

---
+++ greeting.php
@@ -0,0 +1,22 @@
+<?php
+$greeting="こんにちは";
+
+
+$greeting="こんにちは";
+echo $greeting;
+
+
+$greeting="こんばんは";
+echo $greeting;
+/*
+  変数に使える文字　半角英数　_アンダーバー
+
+  echo したものしか画面にでません
+*/
+
+$greeChugoku ="ニーハオ";
+echo "<pre>";
+var_dump($greeChugoku); //str  null(未定義)
+var_dump(true);         //論理値
+var_dump( 1 > 2);
+var_dump( $greeting =="こんばんは");
